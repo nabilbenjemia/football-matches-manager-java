d71af6f19383e7a4b265052c8e7c9108669e7a6e
nabil <nabilbenjemia01@gmail.com>
2024-12-06 22:10:53 +0100

    Adding Admin functionalities in Controller and View

--- src/main/java/Controller/MatchController.java
+++ src/main/java/Controller/MatchController.java
@@ -13,6 +13,9 @@ public class MatchController {
     private boolean isAdministrator;
     private MatchManager matchManager;
 
+    private static final String ADMIN_NAME = "admin123";
+    private static final String ADMIN_PASSWORD = "ilovefootball";
+
     public MatchController(MatchView view, MatchManager matchManager) {
         this.view = view;
         this.matchManager = matchManager;
@@ -33,6 +36,7 @@ public class MatchController {
             case 0 -> registerAsAdministrator();
             case 1 -> viewUpcomingMatches(); //View upcoming matches
             case 2 -> viewOldMatches(); //View Old Matches
+            case 3 -> viewAllMatches(); //view all Matches
             default -> view.showError("Invalid choice. Please try again.");
         }
     }
@@ -51,7 +55,7 @@ public class MatchController {
         LocalDate today = LocalDate.now();
         List<Match> upcomingMatches = matchManager.getMatchesBefore(today); // Filter matches
         if (upcomingMatches.isEmpty()) {
-            view.showMessage("No upcoming matches found.");
+            view.showMessage("No old matches found.");
         } else {
             view.displayMatches(upcomingMatches); // Display matches in the View
         }
@@ -62,9 +66,46 @@ public class MatchController {
     }
 
     public void registerAsAdministrator() {
-        //TODO
+        String[] credentials = view.getAdministratorCredentials();
+        isAdministrator = verifyAdministrator(credentials[0], credentials[1]);
+        if (isAdministrator) {
+            view.showMessage("Welcome " + credentials[0]);
+            handleAdminRequest();
+        } else {
+            view.showError("Please try again! Username or Password are wrong");
+            view.getUserChoice();
+        }
     }
 
+    public boolean verifyAdministrator(String name, String password) {
+        return name.equalsIgnoreCase(ADMIN_NAME) && password.equals(ADMIN_PASSWORD);
+    }
 
+    private void handleAdminRequest() {
+        int adminChoice = view.displayAdminChoice();
+        switch (adminChoice) {
+            case 0 -> addNewMatch();
+            //case 1 ->;
+            case 2 -> changeMatchDay();
+            //case 3 ->;
+            default -> {
+                view.showMessage("Wrong input");
+                view.displayAdminChoice();
+            }
+        }
+        viewAllMatches();
+    }
+
+    //add case of failing parsing
+    private void addNewMatch() {
+        Match match = new Match(view.getOpponent(), LocalDate.parse(view.getMatchDay()), view.getCompetition(), view.isHome());
+        matchManager.addMatch(match);
+
+    }
+
+    private void changeMatchDay() {
+        String[] input = view.getNewMatchDay();
+        matchManager.getMatch(LocalDate.parse(input[0])).setMatchDay(LocalDate.parse(input[1]));
+    }
 
 }
--- src/main/java/View/MatchView.java
+++ src/main/java/View/MatchView.java
@@ -1,7 +1,13 @@
 package View;
 
+import Model.Competition;
 import Model.Match;
+import com.sun.source.tree.BreakTree;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.Console;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,10 +23,78 @@ public class MatchView {
         System.out.println("Please Choose a number : \n" +
                     "- 0 -> Register As an Administrator\n" +
                     "- 1 -> View upcoming matches\n" +
-                    "- 2 -> View Old Matches\n");
+                    "- 2 -> View Old Matches\n" +
+                "- 3 -> View all Matches");
         int input = scanner.nextInt();
 
         return input;
+    }
+
+    public int displayAdminChoice() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please Choose a number : \n" +
+                "- 0 -> Add a Match\n" +
+                "- 1 -> Update result\n" +
+                "- 2 -> Change Date\n" +
+                "- 3 -> Remove Date");
+        int input = scanner.nextInt();
+
+        return input;
+    }
+
+    public String getOpponent() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Opponent: ");
+        String opponent = scanner.nextLine();
+
+        return opponent;
+    }
+
+    public String getMatchDay() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("MatchDay: ");
+        String matchDay = scanner.nextLine();
+        return matchDay;
+    }
+
+    public Competition getCompetition() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Choose a number : \n" +
+                "- 0 -> League\n" +
+                "- 1 -> Cup\n" +
+                "- 2 -> Champions League\n" +
+                "- 3 -> Friendly");
+        int input = scanner.nextInt();
+        Competition competition = null;
+        switch (input) {
+            case 0 -> competition = Competition.LEAGUE;
+            case 1 -> competition = Competition.CUP;
+            case 2 -> competition = Competition.CHAMPIONS_LEAGUE;
+            case 3 -> competition = Competition.FRIENDLY;
+            default -> {
+                System.out.println("Wrong Input");
+                getCompetition();
+            }
+        }
+        return competition;
+    }
+
+    public boolean isHome() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Played at Home?\n" +
+                "Please Choose a number : \n" +
+                "0 -> Home\n" +
+                "1 -> Away");
+        int input = scanner.nextInt();
+        if(input == 0) {
+            return true;
+        } else if(input == 1) {
+            return false;
+        } else {
+            System.out.println("Wrong input");
+        }
+        return false;
     }
 
 
@@ -36,9 +110,43 @@ public class MatchView {
     }
 
     public void displayMatches(List<Match> upcomingMatches) {
-        System.out.println("**Upcomping Matches:** \n");
+        System.out.println("**Results:**");
         for ( Match match: upcomingMatches) {
-            System.out.println(match + "\n");
+            System.out.println(match);
         }
     }
+
+    public String[] getAdministratorCredentials() {
+        String[] credentials = new String[2];
+        Scanner scanner = new Scanner(System.in);
+
+        // Get username
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+        credentials[0] = username;
+
+        // Get password with masking
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        credentials[1] = password;
+
+        return credentials;
+    }
+
+    public String[] getNewMatchDay() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Old Matchday (yyyy-mm-dd");
+        String oldMatchDay = scanner.next();
+        System.out.println("New Matchday (yyyy-mm-dd");
+        String newMatchDay = scanner.next();
+
+        String[] input = new String[2];
+        input[0] = oldMatchDay;
+        input[1] = newMatchDay;
+        return input;
+
+    }
+
+
+
 }
